284340034af29db9c1e4d643b4252fbba2ae92e1
gualterapple <39972000+gualterapple@users.noreply.github.com>
2022-05-02 03:28:55 +0100

    Tratando dos erros na leitura de um arquivo

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function trataErro(erro){
+  throw new Error(chalk.red(erro.code, 'Gualter, o arquivo não foi encontrado'));
+}
+
 function pegaArquivo(caminho)
 {
   const encoding = 'utf-8';
-  fs.readFile(caminho, encoding,(_, data) => {
+  fs.readFile(caminho, encoding,(err, data) => {
+    if(err){
+      trataErro(err);
+    }
     console.log(chalk.green(data));
   });
 }
 
-pegaArquivo('./arquivos/texto1.md');
+pegaArquivo('./arquivos/texo1.md');
