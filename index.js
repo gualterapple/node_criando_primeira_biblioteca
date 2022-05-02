84d6357e1438dfadd7f39ad7a94fc336ca0c0ee5
gualterapple <39972000+gualterapple@users.noreply.github.com>
2022-05-02 04:20:54 +0100

    Capturando link do texto usando regex

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
+
+function extraiLinks(texto){
+  const regex = /\[([^\]]*)\]\(https?:\/\/[^$#\s].[^\s]*\)/gm;
+  const linksExtraidos = texto.match(regex);
+  console.log(chalk.yellow(linksExtraidos));
+}
+
 function trataErro(erro){
   throw new Error(chalk.red(erro.code, 'Gualter, o arquivo não foi encontrado'));
 }
@@ -16,26 +24,5 @@ async function pegaArquivo(caminho)
   }
 }
 
-/*function pegaArquivo(caminho)
-{
-  const encoding = 'utf-8';
-  fs.promises.readFile(caminho, encoding)
-  .then((texto) => {
-    console.log(chalk.bgYellow(texto));
-  })
-  .catch((erro) => {
-    trataErro(erro);});
-}*/
-
-/*function pegaArquivo(caminho)
-{
-  const encoding = 'utf-8';
-  fs.readFile(caminho, encoding,(err, data) => {
-    if(err){
-      trataErro(err);
-    }
-    console.log(chalk.bgYellow(data));
-  });
-}*/
-
-pegaArquivo('./arquivos/texto1.md');
+//pegaArquivo('./arquivos/texto1.md');
+extraiLinks(texto);
